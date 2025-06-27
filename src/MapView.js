3cdf9ae65d6eb9cf185961de51b58cd4d736ffa8
abhi123456781 <72471412+abhi123456781@users.noreply.github.com>
2025-06-27 15:17:57 -0400

    Improve category zoom to center on nearest location

--- src/MapView.js
+++ src/MapView.js
@@ -3,27 +3,20 @@ import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-import foodIconUrl from './icons/food.svg';
-import shelterIconUrl from './icons/shelter.svg';
-import mentalIconUrl from './icons/mental.svg';
-import crisisIconUrl from './icons/crisis.svg';
-import supportIconUrl from './icons/support.svg';
-import healthIconUrl from './icons/health.svg';
-import defaultIconUrl from './icons/default.svg';
-import pinIconUrl from './icons/pin.svg';
-
+// Online icon URLs for each category
 const categoryIcons = {
-  Food: foodIconUrl,
-  Shelter: shelterIconUrl,
-  'Mental Health': mentalIconUrl,
-  'Crisis Support': crisisIconUrl,
-  'Support Services': supportIconUrl,
-  'Health & Wellness': healthIconUrl,
+  Food: "https://cdn-icons-png.flaticon.com/512/1046/1046784.png",
+  Shelter: "https://cdn-icons-png.flaticon.com/512/2271/2271343.png",
+  "Mental Health": "https://cdn-icons-png.flaticon.com/512/3126/3126554.png",
+  "Crisis Support": "https://cdn-icons-png.flaticon.com/512/1828/1828843.png",
+  "Support Services": "https://cdn-icons-png.flaticon.com/512/2460/2460595.png",
+  "Health & Wellness": "https://cdn-icons-png.flaticon.com/512/1484/1484815.png",
+  default: "https://cdn-icons-png.flaticon.com/512/565/565547.png"
 };
 
 const getCategoryIcon = (category) => {
   return new Icon({
-    iconUrl: categoryIcons[category] || defaultIconUrl,
+    iconUrl: categoryIcons[category] || categoryIcons.default,
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -30],
@@ -31,7 +24,7 @@ const getCategoryIcon = (category) => {
 };
 
 const userIcon = new Icon({
-  iconUrl: pinIconUrl,
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
   iconSize: [30, 30],
   iconAnchor: [15, 30],
 });

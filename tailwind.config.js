de8d86b515efd723f154e9311eef1783caa8aace
abhi123456781 <72471412+abhi123456781@users.noreply.github.com>
2025-06-27 17:08:43 -0400

    Save local changes before pulling latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0047AB',
+        primaryDark: '#002f6c',
+        background: '#ffffff',
+      },
+    },
   },
   plugins: [],
 }

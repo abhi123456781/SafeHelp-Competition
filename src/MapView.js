9195f6de33e23ce31cc69f23d5873829be0ae422
abhi123456781 <72471412+abhi123456781@users.noreply.github.com>
2025-06-27 15:32:56 -0400

    Fix Netlify build by removing undefined icon variables

--- src/MapView.js
+++ src/MapView.js
@@ -4,27 +4,27 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 const categoryIcons = {
-  "Food": "https://cdn-icons-png.flaticon.com/512/1046/1046784.png",
-  "Shelter": "https://cdn-icons-png.flaticon.com/512/2271/2271343.png",
+  Food: "https://cdn-icons-png.flaticon.com/512/1046/1046784.png",
+  Shelter: "https://cdn-icons-png.flaticon.com/512/2271/2271343.png",
   "Mental Health": "https://cdn-icons-png.flaticon.com/512/3126/3126554.png",
   "Health & Wellness": "https://cdn-icons-png.flaticon.com/512/1484/1484815.png",
   "Support Services": "https://cdn-icons-png.flaticon.com/512/2460/2460595.png",
   "Crisis Support": "https://cdn-icons-png.flaticon.com/512/1828/1828843.png",
   "Youth Programs": "https://cdn-icons-png.flaticon.com/512/893/893257.png",
   "Community Centers": "https://cdn-icons-png.flaticon.com/512/2769/2769339.png",
-  "Education": "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
-  "Transportation": "https://cdn-icons-png.flaticon.com/512/61/61088.png",
+  Education: "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
+  Transportation: "https://cdn-icons-png.flaticon.com/512/61/61088.png",
   "Senior Services": "https://cdn-icons-png.flaticon.com/512/3103/3103463.png",
-  "Veterans": "https://cdn-icons-png.flaticon.com/512/2965/2965278.png",
+  Veterans: "https://cdn-icons-png.flaticon.com/512/2965/2965278.png",
   "Legal Assistance": "https://cdn-icons-png.flaticon.com/512/929/929430.png",
   "Immigration Support": "https://cdn-icons-png.flaticon.com/512/684/684908.png",
   "Comprehensive Support": "https://cdn-icons-png.flaticon.com/512/2910/2910791.png",
-  "default": "https://cdn-icons-png.flaticon.com/512/565/565547.png"
+  default: "https://cdn-icons-png.flaticon.com/512/565/565547.png"
 };
 
 const getCategoryIcon = (category) => {
   return new Icon({
-    iconUrl: categoryIcons[category] || defaultIconUrl,
+    iconUrl: categoryIcons[category] || categoryIcons.default,
     iconSize: [30, 30],
     iconAnchor: [15, 30],
     popupAnchor: [0, -30],
@@ -32,7 +32,7 @@ const getCategoryIcon = (category) => {
 };
 
 const userIcon = new Icon({
-  iconUrl: pinIconUrl,
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png", // pin icon
   iconSize: [30, 30],
   iconAnchor: [15, 30],
 });
